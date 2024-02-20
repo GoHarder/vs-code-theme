0867303883e8e339907e8562b7f6010500b2bec9
Gregory Harder <goharder83@gmail.com>
2024-02-20 16:15:58 -0600

    Removed deprecated settings.

--- src/dark-default/interface/source-control.js
+++
@@ -1,8 +0,0 @@
-import { tokens } from '../colors/interface.js';
-
-const scm = {
-  // SCM Provider separator border. {default: #454545}
-  providerBorder: tokens.outlineVariant,
-};
-
-export default { 'scm.providerBorder': scm.providerBorder };
