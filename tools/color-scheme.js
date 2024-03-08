bc4a0651cededcde5710f2b37621e734f17534c9
Gregory Harder <goharder83@gmail.com>
2024-03-08 15:57:03 -0600

    Refactored color scheme tool and updated schemas.

--- tools/color-scheme.js
+++ tools/color-scheme.js
@@ -1,13 +1,16 @@
+// @ts-check
+
 import { CorePalette, argbFromHex, hexFromArgb, TonalPalette, Hct, Blend } from '@material/material-color-utilities';
-import { readJsonFile, saveJsonFile } from '../lib/file-system.js';
+import { readDir, readJsonFile, saveJsonFile } from '../lib/file-system.js';
 import chroma from 'chroma-js';
+import inquirer from 'inquirer';
+import jsonSchema from 'json-schema-library';
 
-const file = await readJsonFile('./tools/src/nord.json');
+/** @typedef {import('../schemas/color-source.js').ColorSourceSchema} ColorSource */
 
-const primary = file.interface.primary;
-const secondary = file.interface.secondary;
-const tertiary = file.interface.tertiary;
-const neutral = file.interface.neutral;
+const srcFiles = await readDir('tools/src');
+
+const palette = { $schema: '../../schemas/color-tokens.json/' };
 
 const numberSort = (a, b) => a - b;
 
@@ -18,37 +21,47 @@ const uniqueExtendedTones = [4, 6, 12, 17, 22, 24, 87, 92, 94, 96];
 const standardTones = [...coreTones, ...uniqueStandardTones].sort(numberSort);
 const extendedTones = [...coreTones, ...uniqueExtendedTones].sort(numberSort);
 
-/**
- * @template {string} Prefix
- * @template {number[]} Tones
- * @param {Prefix} prefix Object key prefix.
- * @param {TonalPalette} palette Tonal palette from utils.
- * @param {Tones} tones The tones to create.
- * @return {Record<`${Prefix}${Tones}`, string>}
- */
-const getTones = (prefix, palette, tones) => {
-  const output = {};
-
-  output[prefix] = {};
-
-  return tones.reduce((obj, tone) => {
-    obj[prefix][`_${tone}`] = hexFromArgb(palette.tone(tone));
-    return obj;
-  }, output);
+/** @type {import('inquirer').ListQuestion} */
+const prompt1 = {
+  type: 'list',
+  name: 'srcFile',
+  message: 'Select a source file',
+  choices: srcFiles,
 };
 
 /**
- * @param {string} hex
+ * Asynchronously loads a file using the specified fileName.
+ * @param {string} fileName - The name of the file to be loaded
  */
-const getHueAndChroma = (hex, pri = false) => {
+const loadFile = async (fileName) => {
+  /** @type {ColorSource} */
+  const file = await readJsonFile(`./tools/src/${fileName}`);
+  const rawSchema = await readJsonFile('./schemas/color-source.json');
+
+  const schema = new jsonSchema.Draft07(rawSchema);
+  const errors = schema.validate(file);
+
+  if (errors.length > 0) {
+    console.error(errors);
+    process.exit(1);
+  }
+
+  return file;
+};
+
+/**
+ * Get hue and chroma from hex color.
+ * @param {string} hex Hex color string.
+ * @param {boolean} [adjust] Adjust hue and chroma.
+ */
+const getHueAndChroma = (hex, adjust = false) => {
   const colorInt = argbFromHex(hex);
   const hct = Hct.fromInt(colorInt);
-  let hue = hct.hue;
-  let chroma = hct.chroma;
+  let { hue, chroma } = hct;
 
-  if (pri) {
+  if (adjust) {
+    // @ts-ignore
     const adj = new CorePalette(colorInt, false);
-
     hue = adj.a1.hue;
     chroma = adj.a1.chroma;
   }
@@ -57,124 +70,250 @@ const getHueAndChroma = (hex, pri = false) => {
 };
 
 /**
- * @typedef InterfaceColor
- * @prop {string} color
- * @prop {number} [hueShift]
- * @prop {number} [chromaSplit]
+ * Calculate the secondary hue and chroma based on the primary color and an optional secondary color.
+ * @param {{hue: number, chroma: number}} primary - the primary color object with hue and chroma properties
+ * @param {ColorSource['interface']['secondary']} secondary - optional secondary color object
+ */
+const getSecondaryHueAndChroma = (primary, secondary) => {
+  let hue = primary.hue;
+  let chroma = primary.chroma / 3;
+
+  if (!secondary) return { hue, chroma };
+
+  let { color, hueShift, chromaSplit } = secondary;
+
+  if (color) return getHueAndChroma(color);
+
+  hueShift = hueShift || 0;
+  chromaSplit = chromaSplit || 3;
+
+  hue += hueShift;
+  chroma = chroma / chromaSplit;
+
+  return { hue, chroma };
+};
+
+/**
+ * Calculate the tertiary hue and chroma based on the primary color and an optional tertiary color.
+ * @param {{hue: number, chroma: number}} primary - the primary color object with hue and chroma properties
+ * @param {ColorSource['interface']['secondary']} tertiary - optional tertiary color object
+ */
+const getTertiaryHueAndChroma = (primary, tertiary) => {
+  let hue = primary.hue + 60;
+  let chroma = primary.chroma / 2;
+
+  if (!tertiary) return { hue, chroma };
+
+  let { color, hueShift, chromaSplit } = tertiary;
+
+  if (color) return getHueAndChroma(color);
+
+  hueShift = hueShift || 0;
+  chromaSplit = chromaSplit || 3;
+
+  hue += hueShift;
+  chroma = chroma / chromaSplit;
+
+  return { hue, chroma };
+};
+
+/**
+ * Generates tones for the given palette.
+ * @param {import('@material/material-color-utilities').TonalPalette} palette - The color palette object
+ * @param {number[]} tones - The array of tones to generate
+ * @return {object} The object containing generated tones
+ */
+const getTones = (palette, tones) => {
+  const output = {};
+
+  return tones.reduce((obj, tone) => {
+    obj[`_${tone}`] = hexFromArgb(palette.tone(tone));
+    return obj;
+  }, output);
+};
+
+/**
+ * Build the interface palettes.
+ * @param {ColorSource} source - the color source
+ * @returns {Array<[string, TonalPalette, number[]]>}
+ */
+const buildInterfacePalettes = (source) => {
+  const { primary: p, secondary: s, tertiary: t, neutral: n } = source.interface;
+  const primary = getHueAndChroma(p.value, p.adjustColor);
+  const secondary = getSecondaryHueAndChroma(primary, s);
+  const tertiary = getTertiaryHueAndChroma(primary, t);
+  const error = { hue: 25, chroma: 84 };
+
+  let neutral = { hue: primary.hue, chroma: Math.min(primary.chroma / 12, 4) };
+  let neutralVariant = { hue: primary.hue, chroma: Math.min(primary.chroma / 6, 8) };
+
+  if (n) {
+    const neutralAdj = getHueAndChroma(n.value, n.adjustColor);
+    neutral = { hue: neutralAdj.hue, chroma: Math.min(neutralAdj.chroma / 12, 4) };
+    neutralVariant = { hue: neutralAdj.hue, chroma: Math.min(neutralAdj.chroma / 6, 8) };
+  }
+
+  /** @type {Array<[string, {hue: number, chroma: number}, number[]]>} */
+  const groups = [
+    ['primary', primary, standardTones],
+    ['secondary', secondary, standardTones],
+    ['tertiary', tertiary, standardTones],
+    ['error', error, standardTones],
+    ['neutral', neutral, extendedTones],
+    ['neutralVariant', neutralVariant, standardTones],
+  ];
+
+  return groups.map((group) => {
+    let [tag, { hue, chroma }, tones] = group;
+    const tonalPalette = TonalPalette.fromHueAndChroma(hue, chroma);
+    return [tag, tonalPalette, tones];
+  });
+};
+
+/**
+ * @typedef SyntaxTones
+ * @property {string} base
+ * @property {string} _0
+ * @property {string} _5
+ * @property {string} _10
+ * @property {string} _15
+ * @property {string} _20
+ * @property {string} _25
+ * @property {string} _30
+ * @property {string} _35
+ * @property {string} _40
+ * @property {string} _50
+ * @property {string} _60
+ * @property {string} _70
+ * @property {string} _80
+ * @property {string} _90
+ * @property {string} _95
+ * @property {string} _98
+ * @property {string} _99
+ * @property {string} _100
  */
 
 /**
- * @param {string} pri
- * @param {InterfaceColor} sec
- * @param {InterfaceColor} ter
- * @param {string} [neu]
+ * @typedef SyntaxColor
+ * @property {string} name
+ * @property {SyntaxTones} tones
  */
-const createPalette = (pri, sec, ter, neu) => {
-  const primary = getHueAndChroma(pri, true);
-  let secondary = { hue: primary.hue, chroma: primary.chroma / 3 };
-  let tertiary = { hue: primary.hue + 60, chroma: primary.chroma / 2 };
 
-  if (sec) {
-    if (sec.color) secondary = getHueAndChroma(sec.color);
+/**
+ * Build the syntax palettes.
+ * @param {ColorSource} source - the color source
+ * @returns {Array<SyntaxColor>}
+ */
+const buildSyntaxPalettes = (source) => {
+  const { interface: i, syntax } = source;
 
-    if (sec.hueShift || sec.chromaSplit) {
-      const hueShift = sec?.hueShift ?? 0;
-      const chromaSplit = sec?.chromaSplit ?? 1;
+  let priInt = argbFromHex(i.primary.value);
 
-      secondary = { hue: primary.hue + hueShift, chroma: primary.chroma / chromaSplit };
+  if (i.primary.adjustColor) {
+    // @ts-ignore
+    const adj = new CorePalette(priInt, false);
+    priInt = adj.a1.keyColor.argb;
+  }
+
+  return syntax.map((color) => {
+    const priInt = argbFromHex(i.primary.value);
+    let colorInt = argbFromHex(color.value);
+    if (color.blend) {
+      colorInt = Blend.harmonize(colorInt, priInt);
     }
-  }
 
-  if (ter) {
-    if (ter.color) tertiary = getHueAndChroma(ter.color);
-
-    if (ter.hueShift || ter.chromaSplit) {
-      const hueShift = ter?.hueShift ?? 0;
-      const chromaSplit = ter?.chromaSplit ?? 1;
-
-      tertiary = { hue: primary.hue + hueShift, chroma: primary.chroma / chromaSplit };
-    }
-  }
-
-  const paletteP = TonalPalette.fromHueAndChroma(primary.hue, primary.chroma);
-  const paletteS = TonalPalette.fromHueAndChroma(secondary.hue, secondary.chroma);
-  const paletteT = TonalPalette.fromHueAndChroma(tertiary.hue, tertiary.chroma);
-  const paletteE = TonalPalette.fromHueAndChroma(25, 84);
-  let paletteN = TonalPalette.fromHueAndChroma(primary.hue, Math.min(primary.chroma / 12, 4));
-  let paletteNV = TonalPalette.fromHueAndChroma(primary.hue, Math.min(primary.chroma / 6, 8));
-
-  if (neu) {
-    const neutral = getHueAndChroma(neu);
-    paletteN = TonalPalette.fromHueAndChroma(neutral.hue, Math.min(neutral.chroma / 12, 4));
-    paletteNV = TonalPalette.fromHueAndChroma(neutral.hue, Math.min(neutral.chroma / 6, 8));
-  }
-
-  const paletteGroups = [
-    ['primary', paletteP, standardTones],
-    ['secondary', paletteS, standardTones],
-    ['tertiary', paletteT, standardTones],
-    ['error', paletteE, standardTones],
-    ['neutral', paletteN, extendedTones],
-    ['neutralVariant', paletteNV, standardTones],
-  ];
-
-  return paletteGroups.reduce((obj, group) => {
-    const [prefix, palette, tones] = group;
-
-    const toneClump = getTones(prefix, palette, tones);
-
-    obj = { ...obj, ...toneClump };
-
-    return obj;
-  }, {});
-};
-
-const getOrange = (red, yellow, name) => {
-  const scale = chroma.scale([red.value, yellow.value]);
-  return {
-    value: scale(0.5).hex(),
-    blend: red.blend && yellow.blend,
-    name,
-  };
-};
-
-const createSyntax = (pri, colors) => {
-  const priInt = argbFromHex(pri);
-
-  const red = colors.find((color) => color.name === 'red');
-  const yellow = colors.find((color) => color.name === 'yellow');
-  const brightRed = colors.find((color) => color.name === 'brightRed');
-  const brightYellow = colors.find((color) => color.name === 'brightYellow');
-
-  const orange = getOrange(red, yellow, 'orange');
-  const brightOrange = getOrange(brightRed, brightYellow, 'brightOrange');
-
-  colors = [...colors, orange, brightOrange];
-
-  return colors.reduce((obj, color) => {
-    let value = argbFromHex(color.value);
-    if (color.blend) value = Blend.harmonize(value, priInt);
-
-    const hct = Hct.fromInt(value);
+    const hct = Hct.fromInt(colorInt);
     const hue = hct.hue;
     const chroma = hct.chroma;
 
     const palette = TonalPalette.fromHueAndChroma(hue, chroma);
 
-    let chunk = getTones(color.name, palette, standardTones);
-
-    chunk = { base: color.value, ...chunk[color.name] };
-
-    obj[color.name] = chunk;
-
-    return obj;
-  }, {});
+    return { name: color.name, tones: { base: color.value, ...getTones(palette, standardTones) } };
+  });
 };
 
-const palette = {
-  $schema: './schema.json',
-  interface: createPalette(primary, secondary, tertiary, neutral),
-  terminal: createSyntax(primary, file.syntax),
+/**
+ * Builds a collection of syntax palettes based on the given ColorSource.
+ * @param {ColorSource} source - the color source
+ * @param {SyntaxColor[]} palettes - the syntax colors to generate palettes for
+ */
+const buildTerminal = (source, palettes) => {
+  const { ansi } = source;
+  const output = {};
+
+  for (const key in ansi) {
+    const element = ansi[key];
+    const chunk = palettes.find((p) => p.name === element);
+
+    if (!chunk) {
+      console.log(`Could not find tones for ${key}`);
+      continue;
+    }
+
+    output[key] = chunk.tones;
+  }
+
+  return output;
 };
 
-saveJsonFile('../src/colors/nord.json', palette);
+/**
+ * @param {ColorSource} source
+ * @param {boolean} bright
+ */
+const getOrange = (source, bright) => {
+  const { ansi, syntax } = source;
+  const redKey = bright ? 'brightRed' : 'red';
+  const yellowKey = bright ? 'brightYellow' : 'yellow';
+  const orangeKey = bright ? 'brightOrange' : 'orange';
+  const red = syntax.find((s) => s.name === ansi[redKey]);
+  const yellow = syntax.find((s) => s.name === ansi[yellowKey]);
+
+  if (!red || !yellow) {
+    console.log('Could not find red or yellow');
+    return source;
+  }
+
+  const blend = red.blend && yellow.blend;
+
+  const scale = chroma.scale([red.value, yellow.value]);
+
+  source.syntax.push({
+    name: orangeKey,
+    value: scale(0.5).hex(),
+    blend,
+  });
+
+  source.ansi[orangeKey] = orangeKey;
+
+  return source;
+};
+
+inquirer
+  .prompt([prompt1])
+  .then(async (answers) => {
+    const fileName = answers.srcFile;
+    let fileData = await loadFile(fileName);
+
+    const interfacePallets = buildInterfacePalettes(fileData);
+
+    if (!fileData.ansi.orange) fileData = getOrange(fileData, false);
+    if (!fileData.ansi.brightOrange) fileData = getOrange(fileData, true);
+
+    const syntaxPallets = buildSyntaxPalettes(fileData);
+
+    palette['interface'] = interfacePallets.reduce((obj, [key, palette, tones]) => {
+      obj[key] = getTones(palette, tones);
+      return obj;
+    }, {});
+
+    palette['syntax'] = syntaxPallets;
+    palette['ansi'] = buildTerminal(fileData, syntaxPallets);
+
+    saveJsonFile(`../src/colors/${fileName}`, palette);
+
+    console.log(`Created ${fileName}`);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
